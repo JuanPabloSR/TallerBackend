a529cc341e3fa1c6948bbafe6b95bc1a2af35ac3
Juan Pablo Sánchez Rey <juanpablosanchez744@gmail.com>
2021-11-23 00:16:10 -0500

    v1.0.1 Se realizo el enlace con la base de datos Mysql, agregando la tabla usuarios, adicional se añadieron dependencias para el correcto funcionamiento del programa

---
+++ src/main/java/com/sofka/pruebacrud/PruebacrudApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.pruebacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebacrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebacrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/pruebacrud/PruebacrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.pruebacrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebacrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
