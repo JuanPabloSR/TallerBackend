88ed16db0bd4bc0c1192ef4c0a972db71692355e
Juan Pablo Sánchez Rey <juanpablosanchez744@gmail.com>
2021-11-24 01:04:09 -0500

    v2.1.2 crud finalizado

--- src/main/java/com/sofka/pruebacrud/services/UsuarioService.java
+++ src/main/java/com/sofka/pruebacrud/services/UsuarioService.java
@@ -38,6 +38,8 @@ public class UsuarioService {
         }
     }
 
+    // metodo nuevo para actualizar usuarios
+
     public UsuarioModel update(UsuarioModel usuario){
         return usuarioRepository.save(usuario);
     }
